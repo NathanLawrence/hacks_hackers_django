6e5001fe95be94a608ed3e4e5e21308a19274561
James Gordon <gordon.je@gmail.com>
2015-10-28 19:27:48 -0500

    Added models and admin interface

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Quiz, Question, Choice
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 4
+    max_num = 4
+
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ('quiz', 'text', 'order')
+    list_filter = ['quiz']
+
+admin.site.register(Quiz, QuizAdmin)
+admin.site.register(Question, QuestionAdmin)
