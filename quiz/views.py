52623243507f9b51fa68cb77651ba4b3d710737b
Nathan Lawrence <lawrencen@missouri.edu>
2015-11-25 10:01:08 -0600

    Start of views tutorial.

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import *
+
+#This is Nathan's code. You can probably tell because there are too many comments.
 
 # Create your views here.
+def index(request):
+  return HttpResponse('<h1>Home Page</h1>')
+  
+def quiz_overview(request,quiz_id):
+  #Example of a procedurally constructed (as opposed to templated) view.
+
+  #Sanity check on the input. We want to make sure we're querying an actual quiz -- if we're not, big trouble!!!
+  try:
+  	quiz = Quiz.objects.get(id=quiz_id)
+  except Quiz.DoesNotExist:
+  	return HttpResponseNotFound("<h1>That quiz does not exist.</h1>")
+
+  html = "<h1>Quiz Overview</h1>"
+  return HttpResponse(html)
